ab4d8bb91c3e02e4f803a82ba5cad7beaa71b650
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2018-05-28 15:37:35 +0200

    added spotting controller with search from params

--- app/models/spotting.rb
+++ app/models/spotting.rb
@@ -1,3 +1,14 @@
 class Spotting < ApplicationRecord
   belongs_to :user
+
+  scope :by_longitude, -> (min, max) { min && max ? where('lng >= :min AND lng <= :max', min: min, max: max) : all }
+  scope :by_latitude, -> (min, max) { min && max ? where('lat >= :min AND lat <= :max', min: min, max: max) : all }
+
+  API_RESULTS_LIMIT = 10
+
+  def self.search(min_lat:, max_lat:, min_lng:, max_lng:)
+    by_latitude(min_lat, max_lat).
+      by_longitude(min_lng, max_lng).
+      limit(API_RESULTS_LIMIT)
+  end
 end
